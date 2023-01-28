431374f4dd782fce23e32e9d98e0658b3e95d67b
HamzaGorcevic <gorcevichamza76@gmail.com>
2023-01-28 16:12:37 +0100

    fixed bug

--- app.js
+++ app.js
@@ -26,29 +26,36 @@ console.log(imagesDom, "E");
 console.log(containerImages, "Sda");
 
 right.addEventListener("click", () => {
-  console.log(containerImages.childNodes, "childnodes");
-
   let pom = images[4];
 
   images[4] = images[3];
   images[3] = images[2];
   images[2] = images[1];
   images[1] = images[0];
+
   images[0] = pom;
+  imagesDom[0].style = "opacity:0";
+  console.log(images, "inside");
 
   creatingTable();
+  upperSlider();
 });
 console.log(containerImages.childNodes);
 
 // Lower part
 
 // Upper part
-// let upperSliderVar = document.querySelector(".mainImage");
-// function upperSlider() {
-//   let bigImg = document.createElement("img");
-//   bigImg.src = imagesF()[2];
+let upperSliderVar = document.querySelector(".mainImage");
+function upperSlider() {
+  upperSliderVar.innerHTML = "";
+  let bigImg = document.createElement("img");
+  bigImg.src = images[2];
+  upperSliderVar.append(bigImg);
+  console.log(images[2], "inside upper");
+  console.log(bigImg);
+  console.log(upperSliderVar);
 
-//   upperSliderVar.append(bigImg);
-// }
+  upperSliderVar.append(bigImg);
+}
 
-// upperSlider();
+upperSlider();
